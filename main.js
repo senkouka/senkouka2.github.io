d1862f6244ab9c6917483dc0a9881b8506c4d780
senkouka <124322562+senkouka@users.noreply.github.com>
2023-06-19 11:38:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ window.onload = (e)=>{
 			if(code){
 				drawRect(code.location);// Rect
 				msg.innerText = code.data;// Data
+				location.href = code.date;
 			}else{
 				msg.innerText = "Detecting QR-Code...";
 			}
